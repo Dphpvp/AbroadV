ae5681b7dd845650e64d52397508a1feca9d8364
Dphpvp <122950941+Dphpvp@users.noreply.github.com>
2024-01-14 10:08:58 +0200

    update

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ from home.views import chat_view
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name='home'),
     path('chat/', chat_view, name='chat'),
-    #    path('search/', search_view, name='search'),
+#    path('search/', search_view, name='search'),
     path('', include('about.urls')),
     path('', include('notes.urls')),
     path('', include('utilities.urls')),
